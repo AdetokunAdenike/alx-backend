bc8d9cfdc927b2b9c647cdfc1c4bc2959a37c8ae
AdetokunAdenike <adetokunadenike@gmail.com>
2024-10-28 13:17:57 +0000

    Add helper function

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+ function that takes two integer arguments page and page_size
+"""
+def index_range(page, page_size)
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return(start_index, end_index)
+
+page = 1
+page_size = 2
+result = index_range(page, page_size)
+print(result)
