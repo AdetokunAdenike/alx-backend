a46c16b1756641acd2542517319e0aa76877778c
AdetokunAdenike <adetokunadenike@gmail.com>
2024-10-28 13:43:34 +0000

    Update: update end_index

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -7,5 +7,5 @@ function that takes two integer arguments: page and page_size.
 def index_range(page, page_size):
 
     start_index = (page - 1) * page_size
-    end_index = start_index + page_size
+    end_index = page * page_size
     return (start_index, end_index)
