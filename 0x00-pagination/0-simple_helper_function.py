d9d695ca18855b2b8e94c51cad6f576b5c5b2452
AdetokunAdenike <adetokunadenike@gmail.com>
2024-10-28 13:32:13 +0000

    Update: remove white space

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
- function that takes two integer arguments page and page_size
+function that takes two integer arguments: page and page_size.
 """
 def index_range(page, page_size):
     start_index = (page - 1) * page_size
