cb8ad1e189cb0188a31b1be9d5a183cb8aff0c1f
AdetokunAdenike <adetokunadenike@gmail.com>
2024-10-29 09:17:28 +0000

    Update

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,7 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 function that takes two integer arguments: page and page_size.
 """
+
+from typing import Tuple
 
 
 def index_range(page, page_size):
