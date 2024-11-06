e68d39c29b29db62e58f2ebcdbefbe3d38c7cd32
AdetokunAdenike <adetokunadenike@gmail.com>
2024-11-06 10:20:59 +0100

    Update: Add documentation

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+
+Basic Flask app that renders a "Welcome" page.
+
+"""
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index() -> str:
+    """
+    Renders the index.html page with "Hello world" header.
+    """
+
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
