8051c34a272011e74f7f5e389c544ba3918d0503
AdetokunAdenike <adetokunadenike@gmail.com>
2024-11-09 02:23:12 +0100

    Update

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,26 +1,34 @@
 #!/usr/bin/env python3
 """
-
+Flask app with Babel for intenationalisation.
 """
+
+
 from flask import Flask, ender_template
 from flask_babel import Babel
 
-
-app = Flask(__name__)
-
 class Config:
+    """
+    Config class to set available languages, default locale, and timeszone.
+    """
+
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
-
+app = Flask(__name__)
 app.config.from_object(Config)
-
 babel = Babel(app)
 
 
 @app.route('/')
-def index():
+def index() -> str:
+    """
+    Route that renders the index page.
+    Returns:
+        str: Rendered HTML template for the index page.
+    """
+
     return render_template('1-index.html')
 
 if __name__ == '__main__':
